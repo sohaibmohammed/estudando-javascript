44650dc8b28347aebcdc9ae4c730c839431bc4f1
Sohaib Mohammed <sohaibsalama2021@gmail.com>
2022-10-03 19:14:25 -0300

    aula-01

---
+++ aula-01.js
@@ -0,0 +1,15 @@
+console.log("Mohammed")
+
+console.log(5 + 6)
+console.log(5 * 6)
+console.log(5 < 6)
+console.log(5 <= 5)
+console.log((5 == 5) && (10 < 7))
+console.log((5 == 5) || (10 < 7))
+
+// variaveis
+var name = "Mohammed"  // Não vamos usar (var)
+let age = 40
+const city = "Porto alegre"
+console.log(name, age, city)
+console.log(`Meu nome é ${name}. Minha idade é ${age}. Eu moro em ${city}.`)
